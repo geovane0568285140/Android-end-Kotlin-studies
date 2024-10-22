c21e5f2905dfb444ca3dd8060804d64a5fc44e02
GEOVANE ALMEIDA DA SILVA <geovane08almeida@gmail.com>
2024-10-21 22:17:06 -0400

    add in LoginViewModel the tokenkey end personkey

--- Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -4,14 +4,17 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val person = PersonRepository(application.applicationContext)
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
 
     private val _login = MutableLiveData<ValidationModel>()
     val login: LiveData<ValidationModel> = _login
@@ -24,6 +27,11 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
        person.personLogin(email, password, object : APIListener<PersonModel>{
 
            override fun onSuccess(model: PersonModel) {
+
+               securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, model.personKey)
+               securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, model.personKey)
+               securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, model.name)
+
                _login.value = ValidationModel()
            }
 
