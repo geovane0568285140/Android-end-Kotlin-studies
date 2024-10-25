a19923ac537932c46e8e5eab974cdec101ae0ee7
GEOVANE ALMEIDA DA SILVA <geovane08almeida@gmail.com>
2024-10-24 21:47:35 -0400

    add Priority to the API

--- Fonte/app/src/main/java/com/devmasterteam/tasks/service/model/PriorityModel.kt
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/service/model/PriorityModel.kt
@@ -1,4 +1,26 @@
 package com.devmasterteam.tasks.service.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "Priority")
 class PriorityModel {
+
+    /*
+    {
+        "Id": 1,
+        "Description": "Baixa"
+    },*/
+
+    @SerializedName("Id")
+    @ColumnInfo(name = "id")
+    @PrimaryKey
+    var Id: Int = 0
+
+    @SerializedName("Description")
+    @ColumnInfo(name = "Description")
+    var Description: String = ""
+
 }
---
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/remote/PriorityService.kt
@@ -0,0 +1,15 @@
+package com.devmasterteam.tasks.service.repository.remote
+
+import com.devmasterteam.tasks.service.model.PriorityModel
+import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+
+
+interface PriorityService {
+
+    @GET("Priority")
+    @FormUrlEncoded
+    fun list(): Call<List<PriorityModel>>
+
+}
